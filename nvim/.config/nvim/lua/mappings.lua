f7c51b926f3874686bdefc1886c2f50cd864db1d
maxhu08 <140214354+maxhu08@users.noreply.github.com>
2024-07-22 13:45:26 -0500

    add lsp actions mappings

--- nvim/.config/nvim/lua/mappings.lua
+++ nvim/.config/nvim/lua/mappings.lua
@@ -47,3 +47,17 @@ map("n", "<leader>fr", "<cmd>Telescope oldfiles<cr>", { desc = "fuzzy find recen
 map("n", "<leader>fs", "<cmd>Telescope live_grep<cr>", { desc = "find string in cwd" })
 map("n", "<leader>fc", "<cmd>Telescope grep_string<cr>", { desc = "find string under cursor in cwd" })
 map("n", "<leader>ft", "<cmd>TodoTelescope<cr>", { desc = "find todos" })
+
+-- lsp actions
+map("n", "gD", vim.lsp.buf.declaration, { desc = "go to declaration" }) -- go to declaration
+map("n", "gd", "<cmd>Telescope lsp_definitions<CR>", { desc = "show LSP definitions" }) -- show LSP definitions
+map("n", "gi", "<cmd>Telescope lsp_implementations<CR>", { desc = "show LSP implementations" }) -- show LSP implementations
+map("n", "gt", "<cmd>Telescope lsp_type_definitions<CR>", { desc = "show LSP type definitions" }) -- show LSP type definitions
+map({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, { desc = "see available code actions" }) -- see available code actions, in visual mode will apply to selection
+map("n", "<leader>srn", vim.lsp.buf.rename, { desc = "smart rename" }) -- smart rename
+map("n", "<leader>D", "<cmd>Telescope diagnostics bufnr=0<CR>", { desc = "show buffer diagnostics" }) -- show diagnostics for file
+map("n", "<leader>d", vim.diagnostic.open_float, { desc = "show line diagnostics" }) -- show diagnostics for line
+-- keymap.set("n", "[d", vim.diagnostic.goto_prev, { desc = "go to previous diagnostic" }) -- jump to previous diagnostic in buffer
+-- keymap.set("n", "]d", vim.diagnostic.goto_next, { desc = "go to next diagnostic" }) -- jump to next diagnostic in buffer
+map("n", "g?", vim.lsp.buf.hover, { desc = "show documentation for what is under cursor" }) -- show documentation for what is under cursor
+-- keymap.set("n", "<leader>rs", ":LspRestart<CR>", { desc = "restart LSP" }) -- mapping to restart lsp if necessary
