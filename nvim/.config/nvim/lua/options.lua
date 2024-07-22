3fc14802cef55b7de808235e90df816b3ef237f7
maxhu08 <140214354+maxhu08@users.noreply.github.com>
2024-07-22 13:57:30 -0500

    add opts and more

--- nvim/.config/nvim/lua/options.lua
+++ nvim/.config/nvim/lua/options.lua
@@ -1,6 +1,39 @@
 require "nvchad.options"
 
--- add yours here!
+local opt = vim.opt
 
--- local o = vim.o
--- o.cursorlineopt ='both' -- to enable cursorline!
+-- line numbers
+opt.relativenumber = true
+opt.number = true
+
+-- tabs & indentation
+opt.tabstop = 2
+opt.shiftwidth = 2
+opt.expandtab = true
+opt.autoindent = true
+
+opt.wrap = false
+
+-- search settings
+opt.ignorecase = true
+opt.smartcase = true
+
+opt.cursorline = false
+
+-- stuff
+opt.termguicolors = true
+opt.background = "dark"
+opt.signcolumn = "yes"
+
+-- backspace
+opt.backspace = "indent,eol,start"
+
+-- clipboard
+opt.clipboard:append("unnamedplus")
+
+-- splitting windows
+opt.splitright = true
+opt.splitbelow = true
+
+-- hide ~ on empty line
+opt.fillchars = { eob = " " }
