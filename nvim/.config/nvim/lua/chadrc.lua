6400db814a4380c50e565dd9d6958614b711c46b
maxhu08 <contact@maxhu.dev>
2024-10-17 19:15:16 -0500

    update neovim config

--- nvim/.config/nvim/lua/chadrc.lua
+++ nvim/.config/nvim/lua/chadrc.lua
@@ -1,17 +1,19 @@
--- This file needs to have same structure as nvconfig.lua 
+-- This file needs to have same structure as nvconfig.lua
 -- https://github.com/NvChad/ui/blob/v3.0/lua/nvconfig.lua
--- Please read that file to know all available options :( 
+-- Please read that file to know all available options :(
 
 ---@type ChadrcConfig
 local M = {}
 
 M.base46 = {
-	theme = "tokyonight",
+  theme = "tokyonight",
 
-	hl_override = {
-		Comment = { italic = true },
-		["@comment"] = { italic = true },
-	},
+  hl_override = {
+    Comment = { italic = true },
+    ["@comment"] = { italic = true },
+  },
+
+  transparency = true
 }
 
 return M
