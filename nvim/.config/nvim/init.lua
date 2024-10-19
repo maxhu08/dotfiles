85cb470c07b1c0592d3b64758190429e4c633ef4
maxhu08 <contact@maxhu.dev>
2024-10-18 20:20:41 -0500

    add oil to neovim config

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -49,3 +49,5 @@ vim.o.laststatus = 0
 vim.api.nvim_set_hl(0, "LineNrAbove", { fg = "#737373", bold = false })
 vim.api.nvim_set_hl(0, "LineNr", { fg = "#ffffff", bold = true })
 vim.api.nvim_set_hl(0, "LineNrBelow", { fg = "#737373", bold = false })
+
+require("oil").setup()
---
+++ nvim/.config/nvim/lua/plugins/oil.lua
@@ -0,0 +1,7 @@
+return {
+  'stevearc/oil.nvim',
+  ---@module 'oil'
+  ---@type oil.SetupOpts
+  opts = {},
+  dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if prefer nvim-web-devicons
+}
