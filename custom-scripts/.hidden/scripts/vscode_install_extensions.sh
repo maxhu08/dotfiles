b52ae12cc7a615a4c8a059ebcf37a5a2229caddb
maxhu08 <contact@maxhu.dev>
2026-02-12 18:30:43 -0600

    update extension install script

--- custom-scripts/.hidden/scripts/vscode_install_extensions.sh
+++ custom-scripts/.hidden/scripts/vscode_install_extensions.sh
@@ -1,14 +1,33 @@
 set -- \
+  adpyke.codesnap \
   bradlc.vscode-tailwindcss \
+  davidanson.vscode-markdownlint \
+  dbaeumer.vscode-eslint \
+  donjayamanne.githistory \
+  editorconfig.editorconfig \
   esbenp.prettier-vscode \
+  github.copilot \
+  github.copilot-chat \
+  github.vscode-github-actions \
+  github.vscode-pull-request-github \
+  huntertran.auto-markdown-toc \
   jock.svg \
+  jrieken.vscode-pr-pinger \
+  kuscamara.remove-unused-imports \
+  mechatroner.rainbow-csv \
+  ms-vscode-remote.remote-containers \
+  ms-vscode.vscode-typescript-next \
   murloccra4ler.leap \
+  openai.chatgpt \
   pkief.material-icon-theme \
+  ritwickdey.liveserver \
+  tamasfe.even-better-toml \
+  tomoki1207.pdf \
+  unifiedjs.vscode-mdx \
   usernamehw.errorlens \
   vscodevim.vim \
   wix.vscode-import-cost \
-  github.copilot \
-  github.copilot-chat
+  yoavbls.pretty-ts-errors
 
 for item in "$@"
 do
